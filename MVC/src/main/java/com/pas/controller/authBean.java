c1df6d04c7f2f170540de40c7642931148674ce9
Radek <236607@edu.p.lodz.pl>
2023-01-28 15:36:25 +0100

    how to make auth bean????

--- MVC/src/main/java/com/pas/controller/authBean.java
+++ MVC/src/main/java/com/pas/controller/authBean.java
@@ -1,16 +1,36 @@
 package com.pas.controller;
 
 
+import com.nimbusds.jose.shaded.json.JSONObject;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
 
 
 @ApplicationScoped
 @Named
 public class authBean {
 
-    private String username;
+    @Inject
+    private HttpServletRequest request;
+    private String login;
     private String password;
     private String loginURL = "http://localhost:8080/MVC-1.0-SNAPSHOT/faces/login";
 
+    private String jwt = "";
+    public String login(){
+        JSONObject obj = new JSONObject();
+        obj.put("login", login);
+        obj.put("password", password);
+        String jwt = "trzeba dokonczyc";
+        return "coś";
+    }
+
+    public String logout() throws ServletException {
+        request.logout();
+        jwt = "";
+        return "menu";
+    }
 }
